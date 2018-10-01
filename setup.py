47bec6976ff83c47f29ef74574aa4c2556dcefcc
Jake Lishman <jakelishman@gmail.com>
2018-10-01 13:49:41 +0100

    Update setup to new contact

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
-
+import setuptools
 
 with open('README.md') as f:
     readme = f.read()
@@ -9,14 +6,16 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(
+packages = setuptools.find_packages(exclude=('tests', 'examples'))
+
+setuptool.setup(
     name='floq',
-    version='0.0.1',
+    version='0.1.0',
     description='Floquet control code',
     long_description=readme,
-    author='Marcel Langer',
-    author_email='me@sirmarcel.com',
-    url='https://github.com/sirmarcel/floq',
+    author='Imperial College London CQD Group',
+    author_email='jake.lishman16@imperial.ac.uk',
+    url='https://www.github.com/ImperialCQD/floq',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs', 'benchmark'))
+    packages=packages,
 )
